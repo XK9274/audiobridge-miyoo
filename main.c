1ee8281a8870bfadcc53220ffd0fb90093a76a35
XK <47260768+XK9274@users.noreply.github.com>
2023-05-14 13:53:39 +0100

    Added a buffer & rewind for EOF on stream

--- main.c
+++ main.c
@@ -7,12 +7,36 @@
 #define DEFAULT_FREQ 44100
 #define DEFAULT_CHANNELS 2
 #define AUDIOSPEC_FORMAT AUDIO_S16LSB
+#define BUFFER_SIZE 2048 
 
-void audio_callback(void *userdata, Uint8 *stream, int len) {
-    FILE *file = (FILE*) userdata;
-    while (fread(stream, len, 1, file) != 1) {
-        clearerr(file);  // clear EOF condition
-        SDL_Delay(500);  // wait for more data to become available
+void audio_callback(void* userdata, Uint8* stream, int len) {
+    FILE* file = (FILE*)userdata;
+    static Uint8 buffer[BUFFER_SIZE];
+    static int buffer_index = 0;
+    static int buffer_length = 0;
+    static long marker_position = 0;
+
+    int remaining_length = len;
+    while (remaining_length > 0) {
+        if (buffer_index >= buffer_length) {
+            marker_position = ftell(file);
+            buffer_length = fread(buffer, 1, BUFFER_SIZE, file);
+            if (buffer_length == 0) {
+                clearerr(file); 
+                fseek(file, marker_position, SEEK_SET);
+                SDL_Delay(500); 
+                continue;
+            }
+            buffer_index = 0;
+        }
+
+        int bytes_to_copy = (buffer_length - buffer_index < remaining_length)
+                                ? buffer_length - buffer_index
+                                : remaining_length;
+        memcpy(stream, &buffer[buffer_index], bytes_to_copy);
+        stream += bytes_to_copy;
+        buffer_index += bytes_to_copy;
+        remaining_length -= bytes_to_copy;
     }
 }
 
@@ -23,8 +47,8 @@ int main(int argc, char* argv[]) {
     int c;
 
     // Redirect stdout and stderr to /dev/null
-    freopen("/dev/null", "w", stdout);
-    freopen("/dev/null", "w", stderr);
+    stdout = freopen("/mnt/SDCARD/App/ncspot/logs/log.txt", "w", stdout);
+	stderr = fdopen(dup(fileno(stdout)), "w");
 
     while ((c = getopt(argc, argv, "c:r:")) != -1) {
         switch (c) {
@@ -36,6 +60,7 @@ int main(int argc, char* argv[]) {
                 break;
             default:
                 fprintf(stderr, "Invalid argument -%c\n", c);
+				fflush(stderr);
                 return 1;
         }
     }
@@ -43,10 +68,10 @@ int main(int argc, char* argv[]) {
     spec.freq = freq;
     spec.channels = channels;
     spec.format = AUDIOSPEC_FORMAT;
-    spec.samples = 2048;
+    spec.samples = BUFFER_SIZE;
     spec.callback = audio_callback;
 
-    FILE *file = fdopen(0, "rb");  // read from stdin
+    FILE* file = fdopen(0, "rb"); 
 
     if (file == NULL) {
         fprintf(stderr, "Unable to open input stream!\n");
@@ -57,24 +82,28 @@ int main(int argc, char* argv[]) {
 
     if (SDL_Init(SDL_INIT_AUDIO) < 0) {
         fprintf(stderr, "SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+		fflush(stderr);
         return 1;
     }
 
     if (SDL_OpenAudio(&spec, NULL) < 0) {
-        fprintf(stderr, "Failed to open audio: %s\n", SDL_GetError());
-        return 1;
-    }
+    fprintf(stderr, "Failed to open audio: %s\n", SDL_GetError());
+	fflush(stderr);
+    return 1;
+}
+	fprintf(stderr, "Waiting for audio...!\n");
+	fflush(stderr);
+	SDL_Delay(1000);  // wait for some audio to accumulate in stream before playing
 
-    SDL_PauseAudio(0); // Start audio playback
+	SDL_PauseAudio(0);  // start
 
     while (1) {
-        SDL_Delay(100);  // Let the audio callback work
+        SDL_Delay(100);
     }
 
     SDL_CloseAudio();
     fclose(file);
 
-    // Flush stdout and stderr at the end
     fflush(stdout);
     fflush(stderr);
 
